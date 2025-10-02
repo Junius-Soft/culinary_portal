9c56d27d922131f62cef6918762b5aeacffcd019
mstfrs <mstfrs68@gmail.com>
2025-10-02 20:58:42 +0300

    custom fielkd exported to fixtures

--- culinary_portal/hooks.py
+++ culinary_portal/hooks.py
@@ -8,6 +8,11 @@ app_license = "mit"
 # Apps
 # ------------------
 
+fixtures = [
+    {"dt": "Custom Field", "filters": [["module", "=", "culinary_portal"]]},
+    # {"dt": "Item Group"},
+]
+
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
