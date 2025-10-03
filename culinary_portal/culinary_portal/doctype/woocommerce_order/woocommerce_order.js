49b2442c7f24bdccd302c35d2440cb16bf7a3e8e
mstfrs <mstfrs68@gmail.com>
2025-10-03 13:02:46 +0300

    first commit

---
+++ culinary_portal/culinary_portal/doctype/woocommerce_order/woocommerce_order.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, culinary and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("WooCommerce Order", {
+// 	refresh(frm) {
+
+// 	},
+// });
